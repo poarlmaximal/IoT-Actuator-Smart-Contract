b94c3b7849b56a566102cc6a5d0559f8ec998bb1
poarlmaximal <beyer.paul@gmx.net>
2024-11-24 15:14:41 +0100

    config.py changed

--- Actor-Module/config.py
+++ Actor-Module/config.py
@@ -10,10 +10,10 @@ WIFI_PASSWORD = 'your_wifi_password'  # Replace with your Wi-Fi password
 # MQTT Broker
 MQTT_BROKER_IP_ADDRESS = 'your_mqtt_broker_ip'  # Replace with your MQTT broker IP address
 # MetaMask: Address
-ADDRESS = 'your_metamask_address'  # Replace with your MetaMask address
+ADDRESS = '0x...'  # Replace with your MetaMask address
 # MetaMask: Private Key
-PRIVATE_KEY = '0xyour_private_key'  # Replace with your MetaMask private key
+PRIVATE_KEY = '0x...'  # Replace with your MetaMask private key
 # Smart Contract Address
-CONTRACT_ADDRESS = 'your_smart_contract_address'  # Replace with your smart contract address
+CONTRACT_ADDRESS = '0x...'  # Replace with your smart contract address
 # Infura RPC URL
-RPC_URL = 'https://your_infura_project_id'  # Replace with your Infura RPC URL
+RPC_URL = 'https://sepolia.infura.io/v3/...'  # Replace with your Infura RPC URL
