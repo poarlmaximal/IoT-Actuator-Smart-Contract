6bcad24af676fc894bc0f73b938c3076d0c3a5d2
poarlmaximal <79842901+poarlmaximal@users.noreply.github.com>
2024-12-11 02:16:30 +0100

    Update led_contract.sol

--- Smart-Contract/led_contract.sol
+++ Smart-Contract/led_contract.sol
@@ -4,19 +4,16 @@ pragma solidity ^0.8.0;
 contract LedControl {
     int8 private ledStatus; // 1 = LED an, 0 = LED aus
     address public owner;
-    uint256 public fee; // Fee for setting the LED
+    
 
     // Konstruktor, um den Besitzer des Contracts zu setzen
-    constructor(uint256 _fee) payable {
+    constructor() payable {
         owner = msg.sender;
-        fee = _fee; // Set the fee for setting the LED
     }
 
     // Funktion zum Ein- und Ausschalten der LED
     // Erwartet: 1 für an, 0 für aus
-    
     function setLed(int8 newOn) public payable {
-    require(msg.value >= fee, "Insufficient fee sent for setting LED");
         require(newOn == 0 || newOn == 1, "Ungueltiger Wert fuer LED");
         ledStatus = newOn;
     }
